336a21fa3e85ba9ab3f3c1b31f82c67e52126742
name <abc@def.com>
2023-05-09 17:43:00 +0900

    chap10

--- chap10/1.py
+++ chap10/1.py
@@ -2,10 +2,10 @@ import sys
 
 print(sys.builtin_module_names)
 
-import numpy
+import numpy as np
 
-arrA = numpy.array([1,2,3,4,5])
-arrB = numpy.array([6,7,8,9,10])
+arrA = np.array([1,2,3,4,5])
+arrB = np.array([6,7,8,9,10])
 
 print(arrA + arrB)
 
