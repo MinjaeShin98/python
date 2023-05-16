6835cc6e122778f7c529e42698725e8f3474d28e
shin <shin@naver.com>
2023-05-16 11:23:37 +0900

    python

---
+++ chap11/1.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selinium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
